1084749477567532eaf63f949cf0ca4f90f69ac5
nevin-cleetus <61216766+nevin-cleetus@users.noreply.github.com>
2020-10-01 17:40:26 +0530

    Update bootstrap.sh

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -58,6 +58,9 @@ EOF
 # Install Kubernetes
 echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
 yum install -y -q kubeadm kubelet kubectl >/dev/null 2>&1
+## yum install -y -q kubeadm-1.9.0 kubelet-1.9.0 kubectl-1.9.0 > /dev/null 2>&1
+
+
 
 # Start and Enable kubelet service
 echo "[TASK 10] Enable and start kubelet service"
